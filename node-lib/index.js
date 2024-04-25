3e76ac8ed9f25e78662f0bca9917e7dc4b190695
Gabriel Freitas Dev <gabrielfreitas.ilustra@gmail.com>
2024-04-25 20:05:54 -0300

    handle errors

--- node-lib/index.js
+++ node-lib/index.js
@@ -1,6 +1,18 @@
-import chalk from 'chalk';
+import fs from "fs"
+import chalk from "chalk"
 
-console.log(chalk.blue('Hello world!'));
+function handleError(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'))
+}
 
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement).');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement).');
+function getFile(pathFile) {
+    const enconding = "utf-8"
+    fs.readFile(pathFile, enconding, (erro, text) => {
+        if (erro) {
+            handleError(erro)
+        }
+        console.log(chalk.green(text))
+    })
+}
+
+getFile('./arquivos/texto.md')
