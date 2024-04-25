2b7d035e4325f661fc67a7b6b5c12642b4a6a08a
Gabriel Freitas Dev <gabrielfreitas.ilustra@gmail.com>
2024-04-25 20:43:31 -0300

    async/await

--- node-lib/index.js
+++ node-lib/index.js
@@ -5,6 +5,20 @@ function handleError(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'))
 }
 
+async function getFile(pathFile) {
+    try {
+        const enconding = 'utf-8'
+        const text = await fs.promises.readFile
+            (pathFile, enconding)
+        console.log(chalk.green(text))
+    } catch (erro) {
+        handleError(erro)
+    }
+}
+
+/*
+promise com then()
+
 function getFile(pathFile) {
     const enconding = 'utf-8'
     fs.promises
@@ -12,4 +26,7 @@ function getFile(pathFile) {
         .then((text) => console.log(chalk.green(text)))
         .catch(handleError)
 }
+*/
 
+getFile('./arquivos/texto.md')
+getFile('./arquivos/')
